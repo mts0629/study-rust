49748d10ec42f752e89e537a0ae9e1cce5bbfb5f
mts0629 <soratobi93@gmail.com>
2023-12-31 17:13:27 +0900

    The Book: 7.3

--- the_book/07_packages_crates_and_modules/restaurant/src/lib.rs
+++ the_book/07_packages_crates_and_modules/restaurant/src/lib.rs
@@ -1,8 +1,8 @@
 // Define a module: `front_of_house`
 mod front_of_house {
     // Submodule: `hosting`
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -15,4 +15,58 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,      // Accessible
+        seasonal_fruit: String, // Not accessible
+    }
+
+    // Accessible
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        // Refer a parent module
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+// Function which refers a crate's function
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Won't be compile because seasonal_fruit is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // Access to a public enum
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
