7b0b6186453264a760b7dabb27bbb5f980517a7a
mts0629 <soratobi93@gmail.com>
2024-11-06 23:59:38 +0900

    Dice: pass the number of rolls

--- playground/dice/src/main.rs
+++ playground/dice/src/main.rs
@@ -1,7 +1,18 @@
+use std::env;
+
 use rand::Rng;
 
 fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let rolls: u32 = match &args[0].trim().parse() {
+        Ok(num) => *num,
+        Err(_) => return,
+    };
+
     let sides: i32 = 6;
 
-    println!("{}", rand::thread_rng().gen_range(1..(sides + 1)));
+    for _ in 0..rolls {
+        println!("{}", rand::thread_rng().gen_range(1..(sides + 1)));
+    }
 }
