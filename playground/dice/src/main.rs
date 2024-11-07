c3a7bf4777302661fe12eb04b133a08a21c39d1b
mts0629 <soratobi93@gmail.com>
2024-11-07 23:57:01 +0900

    Dice: roll x1 without argument

--- playground/dice/src/main.rs
+++ playground/dice/src/main.rs
@@ -5,12 +5,16 @@ use rand::Rng;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let rolls: u32 = match &args[0].trim().parse() {
-        Ok(num) => *num,
-        Err(_) => return,
+    let rolls: u32 = if args.len() == 0 {
+        1
+    } else {
+        match &args[0].trim().parse() {
+            Ok(num) => *num,
+            Err(_) => return,
+        }
     };
 
-    let sides: i32 = 6;
+    let sides: u32 = 6;
 
     for _ in 0..rolls {
         println!("{}", rand::thread_rng().gen_range(1..(sides + 1)));
