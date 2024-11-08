987c06fc2c74656ac8219c6760805c941a11028b
mts0629 <soratobi93@gmail.com>
2024-11-08 23:53:17 +0900

    Dice: use clap crate

--- playground/dice/src/main.rs
+++ playground/dice/src/main.rs
@@ -1,22 +1,20 @@
-use std::env;
-
+use clap::Parser;
 use rand::Rng;
 
-fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+/// Rolling dices
+#[derive(Parser, Debug)]
+struct Args {
+    /// Number of rolls
+    #[arg(short, long, default_value_t = 1)]
+    rolls: u32,
+}
 
-    let rolls: u32 = if args.len() == 0 {
-        1
-    } else {
-        match &args[0].trim().parse() {
-            Ok(num) => *num,
-            Err(_) => return,
-        }
-    };
+fn main() {
+    let args = Args::parse();
 
     let sides: u32 = 6;
 
-    for _ in 0..rolls {
+    for _ in 0..args.rolls {
         println!("{}", rand::thread_rng().gen_range(1..(sides + 1)));
     }
 }
