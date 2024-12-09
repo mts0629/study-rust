9a59a9958948bbcb9bf3714268c7b7ca1217fd3c
mts0629 <soratobi93@gmail.com>
2024-12-09 23:30:06 +0900

    The Book: 11.2

--- the_book/11_writing_automated_tests/adder/src/lib.rs
+++ the_book/11_writing_automated_tests/adder/src/lib.rs
@@ -1,8 +1,10 @@
+use std::{thread, time};
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-fn add_two(a: usize) -> usize {
+pub fn add_two(a: usize) -> usize {
     a + 2
     // a + 3 // Bug
 }
@@ -37,5 +39,38 @@ mod tests {
     fn it_adds_two() {
         let result = add_two(2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
+    }
+
+    #[test]
+    // Ignore this test,
+    // run ignored tests with `cargo test -- --ignored` and
+    // run all tests with `cargo test -- --include-ignored`
+    #[ignore]
+    fn expensive_test() {
+        // Code that takes time...
+        let one_hundred_sec = time::Duration::from_secs(100);
+        let now = time::Instant::now();
+
+        thread::sleep(one_hundred_sec);
+
+        assert!(now.elapsed() >= one_hundred_sec);
     }
 }
