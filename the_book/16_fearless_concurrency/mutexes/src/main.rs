78e6acd4954e29bb157bf2f7846a8b8b6d7e8ed1
mts0629 <soratobi93@gmail.com>
2025-06-21 12:08:49 +0900

    The Book: 16.3, thread-safe access by Arc

--- the_book/16_fearless_concurrency/mutexes/src/main.rs
+++ the_book/16_fearless_concurrency/mutexes/src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+// use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
@@ -13,7 +14,7 @@ fn main() {
 
     println!("m = {m:?}"); // Show a value and "poisoning"
 
-    // Compilation failed,
+    // Compilation failed
     // because ownership of a mutex's lock can't be moved into multiple threads
     /*let counter = Mutex::new(0);
     let mut handles = vec![];
@@ -33,4 +34,46 @@ fn main() {
     }
 
     println!("Result: {}", *counter.lock().unwrap());*/
+
+    // Compilation failed
+    // thread-unsafe, access to counter may be interrupted by other threads
+    /* let counter = Rc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    }
+
+    println!("Result: {}", *counter.lock().unwrap()); */
+
+    // Create an atomic reference count
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            // Thread-safe access
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
