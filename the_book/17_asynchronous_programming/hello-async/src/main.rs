8803a6e1b0c606cde33a3c745fd9b5ead768441c
mts0629 <soratobi93@gmail.com>
2025-06-24 23:42:04 +0900

    The Book: 17.1, racing

--- the_book/17_asynchronous_programming/hello-async/src/main.rs
+++ the_book/17_asynchronous_programming/hello-async/src/main.rs
@@ -1,5 +1,5 @@
 // trpl: support crate of "the book"
-use trpl::Html;
+use trpl::{Either, Html};
 
 // Return a text of the title element from requested page URL
 // executed asynchronously
@@ -21,15 +21,42 @@ async fn page_title(url: &str) -> Option<String> {
 //     }
 // }
 
+async fn page_url_and_title(url: &str) -> (&str, Option<String>) {
+    let text = trpl::get(url).await.text().await;
+    let title = Html::parse(&text)
+        .select_first("title")
+        .map(|title| title.inner_html());
+    (url, title)
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let url = &args[1];
+
+    /*let url = &args[1];
 
     trpl::run(async {
         // async functions must be called in async blocks
         match page_title(url).await {
             Some(title) => println!("The title of {url} was {title}"),
             None => println!("{url} had no title"),
+        }
+    })*/
+
+    // Print the title of the first-returned URL from 2 URLs
+    trpl::run(async {
+        let title_fut_1 = page_url_and_title(&args[1]);
+        let title_fut_2 = page_url_and_title(&args[2]);
+
+        // Get the first-returned value of two by race
+        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title is: '{title}'"),
+            None => println!("Its title could not be parsed."),
         }
     })
 }
