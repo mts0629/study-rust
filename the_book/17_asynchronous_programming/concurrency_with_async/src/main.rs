3c595354506fedd822e0792db2ffbacbcf0346ce
mts0629 <soratobi93@gmail.com>
2025-06-26 23:59:22 +0900

    The Book: 17.2, async channels

--- the_book/17_asynchronous_programming/concurrency_with_async/src/main.rs
+++ the_book/17_asynchronous_programming/concurrency_with_async/src/main.rs
@@ -18,6 +18,8 @@ fn main() {
         handle.await.unwrap();
     });
 
+    println!("***");
+
     // Join futures
     // execution is the exect same order every time by runtime's control
     trpl::run(async {
@@ -36,5 +38,64 @@ fn main() {
         };
 
         trpl::join(fut1, fut2).await;
-    })
+    });
+
+    println!("***");
+
+    // Async channel
+    trpl::run(async {
+        let (tx, mut rx) = trpl::channel(); // Receiver is mutable
+
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+
+        let received = rx.recv().await.unwrap();
+        println!("Got: {received}");
+    });
+
+    println!("***");
+
+    // Async multi channels
+    trpl::run(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone(); // cloned before move
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        }; // tx1 is droped here, trigger rx closing
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                // rx closes when senders are dropped
+                println!("received '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        }; // tx is dropped here, trigger rx closing
+
+        // Join channels
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+    });
 }
