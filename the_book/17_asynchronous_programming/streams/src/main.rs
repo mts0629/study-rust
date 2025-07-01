35e312c95faf188148ba28d44d0a4ef04693d583
mts0629 <soratobi93@gmail.com>
2025-07-01 23:59:33 +0900

    The Book: 17.4, composing streams

--- the_book/17_asynchronous_programming/streams/src/main.rs
+++ the_book/17_asynchronous_programming/streams/src/main.rs
@@ -1,4 +1,23 @@
-use trpl::StreamExt;
+use std::{pin::pin, time::Duration};
+use trpl::{ReceiverStream, Stream, StreamExt};
+
+// Return a stream of String
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (index, message) in messages.into_iter().enumerate() {
+            // Sleep 100 ms/300 ms by turns
+            let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
+            trpl::sleep(Duration::from_millis(time_to_sleep));
+
+            tx.send(format!("Message: '{message}'")).unwrap();
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
 
 fn main() {
     trpl::run(async {
@@ -12,6 +31,31 @@ fn main() {
 
         while let Some(value) = filtered.next().await {
             println!("The value was: {value}");
+        }
+    });
+
+    println!("***");
+
+    trpl::run(async {
+        let mut messages = get_messages();
+
+        // Get messages from stream
+        while let Some(message) = messages.next().await {
+            println!("{message}");
+        }
+    });
+
+    println!("***");
+
+    trpl::run(async {
+        let mut messages = pin!(get_messages().timeout(Duration::from_millis(200)));
+
+        // Timeout occurs while getting messages from stream, but it don't stop
+        while let Some(result) = messages.next().await {
+            match result {
+                Ok(message) => println!("{message}"),
+                Err(reason) => eprintln!("Problem: {reason:?}"),
+            }
         }
     });
 }
