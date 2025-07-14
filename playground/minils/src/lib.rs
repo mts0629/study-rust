3b78846a447e31104e373f69ac239fa587e7b4f4
mts0629 <soratobi93@gmail.com>
2025-07-14 23:39:56 +0900

    minils: print results of multiple paths

--- playground/minils/src/lib.rs
+++ playground/minils/src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::io::Write;
 
 // Get path strings from arguments
 pub fn get_paths(args: &[String]) -> Vec<String> {
@@ -30,16 +31,41 @@ fn get_dir_entries(path: &String) -> Result<Vec<String>, Box<dyn Error>> {
 }
 
 // Print entries
-fn print_entries(entries: Vec<String>) {
-    println!("{}", entries.join("  "));
+fn print_entries<W: Write>(
+    mut writer: W,
+    path: &String,
+    entries: Vec<String>,
+    print_path: bool,
+) -> Result<(), Box<dyn Error>> {
+    if print_path {
+        writeln!(writer, "{}:", path)?;
+    }
+    writeln!(writer, "{}", entries.join("  "))?;
+
+    Ok(())
+}
+
+// Main process
+fn ls<W: Write>(mut writer: W, paths: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    let print_path = paths.len() > 1;
+
+    let mut iter = paths.iter().peekable();
+
+    while let Some(path) = iter.next() {
+        let dir_entries = get_dir_entries(&path)?;
+
+        print_entries(&mut writer, &path, dir_entries, print_path)?;
+
+        if !iter.peek().is_none() {
+            writeln!(writer, "")?;
+        }
+    }
+
+    Ok(())
 }
 
 pub fn run(paths: Vec<String>) -> Result<(), Box<dyn Error>> {
-    let dir_entries = get_dir_entries(&paths[0])?;
-
-    print_entries(dir_entries);
-
-    Ok(())
+    ls(std::io::stdout(), &paths)
 }
 
 #[cfg(test)]
@@ -64,6 +90,42 @@ mod tests {
             Err(err) => {
                 assert_eq!(err.to_string(), "No such file or directory (os error 2)");
             }
+        }
+    }
+
+    #[test]
+    fn formatted_print() {
+        let list = vec![String::from("a.txt"), String::from("b.txt")];
+        let mut buf = Vec::new();
+
+        match print_entries(&mut buf, &String::from("."), list, false) {
+            Ok(_) => assert_eq!(String::from_utf8(buf).unwrap(), "a.txt  b.txt\n"),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn formatted_print_with_path() {
+        let list = vec![String::from("a.txt"), String::from("b.txt")];
+        let mut buf = Vec::new();
+
+        match print_entries(&mut buf, &String::from("."), list, true) {
+            Ok(_) => assert_eq!(String::from_utf8(buf).unwrap(), ".:\na.txt  b.txt\n"),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn print_paths() {
+        let paths = vec![String::from("."), String::from("src")];
+        let mut buf = Vec::new();
+
+        match ls(&mut buf, &paths) {
+            Ok(_) => assert_eq!(
+                String::from_utf8(buf).unwrap(),
+                ".:\nCargo.lock  Cargo.toml  src  target\n\nsrc:\nlib.rs  main.rs\n"
+            ),
+            Err(_) => assert!(false),
         }
     }
 }
