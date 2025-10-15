bc5beceb5c5a4d430e1380f29ce0c725f54e93e1
mts0629 <soratobi93@gmail.com>
2025-10-15 23:59:49 +0900

    The Book: 19.2, refutable/irrefutable patterns

--- the_book/19_pattern_and_matching/refutability/src/main.rs
+++ the_book/19_pattern_and_matching/refutability/src/main.rs
@@ -1,8 +1,38 @@
 fn main() {
-    // Pattern `Some(x)` is refutable, but `let` accepts only irrefutable patterns
-    // let Some(x) = Some(1);
-    let Some(x) = Some(1) else { return }; // OK
+    // Irrefutable patterns
+    // `let`
+    let x = 5;
 
-    // Irrefutable pattern for `if let`
-    let x = 5 else { return };
+    // `for`
+    for i in 1..6 {
+        println!("{}", i)
+    }
+
+    // Function arguments
+    fn _add(x: i32) -> i32 {
+        x + 1
+    }
+
+    let mut num = Some(5);
+    // Refutable patterns
+    // `if let`
+    if let Some(x) = num { println!("num = 5"); }
+
+    // `while let`
+    while let Some(x) = num {
+        if x > 6 {
+            println!("quit");
+            break;
+        } else {
+            println!("num = {}", x);
+            num = Some(x + 1);
+        }
+    }
+
+    // Pattern `Some(x)` is refutable, but `let` accepts only irrefutable patterns
+    // let Some(x) = num; // NG
+    let Some(x) = num else { return }; // OK
+
+    // Irrefutable pattern for `let else` will be warned
+    let x = 5 else { return; };  // warning: irrefutable `let...else` pattern
 }
