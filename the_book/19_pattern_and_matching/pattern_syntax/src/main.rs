0413d564eda65c95ef616a57c816e41b4492cafb
mts0629 <soratobi93@gmail.com>
2025-10-17 23:22:24 +0900

    The Book: 19.3, destructuring values

--- the_book/19_pattern_and_matching/pattern_syntax/src/main.rs
+++ the_book/19_pattern_and_matching/pattern_syntax/src/main.rs
@@ -70,4 +70,57 @@ fn main() {
             println!("On neither axis: ({x}, {y})");
         }
     }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    // Destructing enums
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+    }
+
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum NewMessage {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = NewMessage::ChangeColor(Color::Hsv(0, 160, 255));
+
+    // Destructuring nested structs and enums
+    match msg {
+        NewMessage::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        NewMessage::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {h}, and value {v}");
+        }
+        _ => (),
+    }
+
+    // Destructuring structs and tuples
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
 }
