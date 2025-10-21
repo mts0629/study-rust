759cfb10730c95d950095619280812e5fd67fabd
mts0629 <soratobi93@gmail.com>
2025-10-21 23:52:03 +0900

    The Book: 20.1, static vars, unsafe trait, union

--- the_book/20_advanced_features/unsafe_rust/src/main.rs
+++ the_book/20_advanced_features/unsafe_rust/src/main.rs
@@ -29,6 +29,34 @@ unsafe extern "C" {
     safe fn abs(input: i32) -> i32;
 }
 
+// Static variable
+static HELLO_WORLD: &str = "Hello, world!";
+
+static mut COUNTER: u32 = 0;
+
+/// SAFETY: Calling this from more than a single thread at a time is undefined
+/// behavior, so you *must* guarantee you only call it from a single thread at
+/// a time
+unsafe fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+// Unsafe trait
+unsafe trait Foo {
+    // Methods go here
+}
+
+unsafe impl Foo for i32 {
+    // Method implementation go here
+}
+
+union MyUnion {
+    f1: u32,
+    f2: f32,
+}
+
 fn main() {
     let mut num = 5;
 
@@ -72,4 +100,19 @@ fn main() {
     }
 
     println!("Absolute value of -3 according to C: {}", abs(-3));
+
+    println!("name is: {HELLO_WORLD}");
+
+    unsafe {
+        /// SAFETY: This is only called from a single thread in `main`
+        add_to_count(3);
+        println!("COUNTER: {}", *(&raw const COUNTER));
+    }
+
+    let u = MyUnion { f1: 1 };
+
+    unsafe {
+        // Access fields of union
+        println!("{}", u.f1);
+    }
 }
