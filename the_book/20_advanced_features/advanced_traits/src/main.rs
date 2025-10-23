b445c76cec9430bacfdfab63e3836aa64ef1fc52
mts0629 <soratobi93@gmail.com>
2025-10-23 23:59:52 +0900

    The Book: 20.2, operator overloading

--- the_book/20_advanced_features/advanced_traits/src/main.rs
+++ the_book/20_advanced_features/advanced_traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 struct Counter {
     count: u32,
 }
@@ -12,6 +14,40 @@ impl Iterator for Counter {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// Add trait has default generic type parameter
+// for Rhs (righthand-side operand)
+// trait Add<Rhs=Self> { ... }
+impl Add for Point {
+    type Output = Point;
+
+    // Overload '+' operator
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    // Overload '+' operator for Millimeters, to add Meters
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
 fn main() {
     let mut counter = Counter { count: 0 };
 
@@ -22,4 +58,13 @@ fn main() {
             None => {}
         }
     }
+
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 },
+    );
+
+    let m = Meters { 0: 1 };
+    let mm = Millimeters { 0: 100 };
+    println!("{:?}", mm + m);
 }
