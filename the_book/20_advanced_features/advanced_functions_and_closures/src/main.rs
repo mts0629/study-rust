11f3a7157f615666437faf71a109927bf640d417
mts0629 <soratobi93@gmail.com>
2025-10-31 23:58:22 +0900

    The Book: 20.4, returning closures

--- the_book/20_advanced_features/advanced_functions_and_closures/src/main.rs
+++ the_book/20_advanced_features/advanced_functions_and_closures/src/main.rs
@@ -7,6 +7,20 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+// Function which returns a closure
+fn returns_closure() -> impl Fn(i32) -> i32 {
+    |x| x + 1
+}
+
+// Two functions return a closure with same signature but different implementation
+// Can operate as the same type with Box
+fn returns_closure_b() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+fn returns_initialized_closure(init: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(move |x| x + init)
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
 
@@ -27,4 +41,11 @@ fn main() {
 
     // Use an enum initializer functions in map
     let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    // Iterate a vector of closures
+    let handlers = vec![returns_closure_b(), returns_initialized_closure(123)];
+    for handler in handlers {
+        let output = handler(5);
+        println!("{output}");
+    }
 }
