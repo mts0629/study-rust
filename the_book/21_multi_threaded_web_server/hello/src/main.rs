32bcc93a47a567a5707f8aaefb5a21558bbae98d
mts0629 <soratobi93@gmail.com>
2025-11-06 23:48:01 +0900

    The Book: 21.1, reading the request

--- the_book/21_multi_threaded_web_server/hello/src/main.rs
+++ the_book/21_multi_threaded_web_server/hello/src/main.rs
@@ -1,11 +1,27 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
+    // Listen at a local address
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+    // Print a data in the TCP stream
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
