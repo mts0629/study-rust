d8a42888d37065163464433d440985053df629a7
mts0629 <soratobi93@gmail.com>
2025-11-07 23:54:23 +0900

    The Book: 21.1: returning an HTML response

--- the_book/21_multi_threaded_web_server/hello/src/main.rs
+++ the_book/21_multi_threaded_web_server/hello/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
@@ -14,14 +15,26 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
-    // Print a data in the TCP stream
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+
+    // Get an HTTP request
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    // Print a data in the TCP stream
+    // println!("Request: {http_request:#?}");
+
+    // Create an HTTP response
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
