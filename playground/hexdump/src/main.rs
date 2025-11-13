b821852346418f79a2d5db081cfa285c0c0f271e
mts0629 <soratobi93@gmail.com>
2025-11-13 23:52:53 +0900

    hexdump: print a file content

--- playground/hexdump/src/main.rs
+++ playground/hexdump/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = match args.len() {
+        2 => String::from(args[1].clone()),
+        _ => {
+            eprintln!("Error: file is not specified");
+            process::exit(1);
+        }
+    };
+
+    match fs::read_to_string(file_path) {
+        Ok(content) => {
+            print!("{content}");
+        }
+        Err(err) => {
+            eprintln!("Error: {err}");
+            process::exit(1);
+        }
+    }
 }
