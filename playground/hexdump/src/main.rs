2479b00c5cd78e9023a5e25183f2a3d96f2588d4
mts0629 <soratobi93@gmail.com>
2025-11-15 23:59:37 +0900

    hexdump: split to modules

--- playground/hexdump/src/main.rs
+++ playground/hexdump/src/main.rs
@@ -1,46 +1,69 @@
 use std::env;
 use std::fs;
+use std::io::{BufRead, BufReader};
 use std::process;
 
-fn print_hex(content: Vec<u8>, col_size: u32) {
-    let mut byte_index = 0;
+fn get_file_path() -> Option<String> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        None
+    } else {
+        Some(String::from(args[1].clone()))
+    }
+}
+
+fn print_hex(file_path: &str, col_size: u32) -> Result<(), String> {
+    let f = match fs::File::open(file_path) {
+        Ok(f) => f,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    let reader = BufReader::new(f);
+
+    let mut ofs = 0;
     let mut col = 0;
-    for byte in content {
-        if col == 0 {
-            print!("0x{byte_index:04x} | ");
-        }
+    for line in reader.lines() {
+        let bytes = match line?.into_iter().collect::<Result<Vec<_>, _>>() {
+            Ok(bytes) => bytes,
+            Err(err) => return Err(err.to_string()),
+        };
 
-        print!("{byte:02x} ");
-        col += 1;
-        byte_index += 1;
+        for byte in bytes {
+            if col == 0 {
+                print!("0x{ofs:04x} | ");
+            }
 
-        if col == col_size {
-            println!("");
-            col = 0
+            print!("{byte:02x} ");
+            col += 1;
+            ofs += 1;
+
+            if col == col_size {
+                println!("");
+                col = 0
+            }
         }
     }
 
     println!("");
+
+    Ok(())
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let file_path = match args.len() {
-        2 => String::from(args[1].clone()),
-        _ => {
+    let file_path = match get_file_path() {
+        Some(file_path) => file_path,
+        None => {
             eprintln!("Error: file is not specified");
             process::exit(1);
         }
     };
 
-    let content = match fs::read(file_path) {
-        Ok(content) => content,
+    match print_hex(&file_path, 16) {
         Err(err) => {
             eprintln!("Error: {err}");
             process::exit(1);
         }
+        _ => {}
     };
-
-    print_hex(content, 16);
 }
